2377f59db2cf8e05305f783e62f5047935127611
yaroslav466 <ignatovjaroslaw@gmail.com>
2023-08-31 02:23:35 +0200

    commit3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
+
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+
+const renderList = (arr, container) =>{ 
+    const markup = arr.map((item) => `<li class="gallery_item"> 
+    <a class="gallery_link" href="${item.original}">
+    <img
+        class="gallery_image"
+        src="${item.preview}"
+        alt="${item.description}"
+        width="360"
+        />
+    </a>
+    </li>`).join("");
+    
+    container.insertAdjacentHTML("afterbegin", markup);
+}
+
+renderList(galleryItems, listEl);
+
+ const gallery = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionType: 'attr', 
+        captionsData: 'alt', 
+        animationSpeed: 250,
+});
